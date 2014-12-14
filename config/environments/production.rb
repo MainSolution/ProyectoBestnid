2f8265fc00a4a5c1d8d9f5ad11e7807e0dbb172f
martin <md.cantarini@gmail.com>
2014-12-14 17:22:25 -0200

    end7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,8 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts');
+  config.assets.precompile += %w( .svg .eot .woff .ttf );
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
