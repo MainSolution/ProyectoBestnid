f0115b9cf602c103103f42ea63a8e473a3ce9061
martin <md.cantarini@gmail.com>
2014-12-14 16:03:18 -0200

    end5

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.paths << Rails.root.join("app", "assets", "fonts")
+config.assets.precompile += %w(.svg .eot .woff .ttf)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 module BestnidOficial
   class Application < Rails::Application
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
