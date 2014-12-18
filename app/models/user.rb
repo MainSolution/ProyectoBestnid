eb3f1526c2415855ef0320b7d4241f35fd11b4da
martin <md.cantarini@gmail.com>
2014-12-18 18:37:38 -0200

    finala2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ has_many :offers
 
 ROLES = %w[admin user]
 #validates_presence_of :nombre, :apellido, :nick , :password, :password_confirmation, :email
-validates_uniqueness_of :nick , :email 
-validates_length_of :password, minimum: 8
+#validates_uniqueness_of :nick , :email 
+#validates_length_of :password, minimum: 8
 #validar el formato de correo 
 mount_uploader :foto, FotoUploader 
 
